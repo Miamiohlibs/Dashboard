3306bb45e3c8bd3d06304b443f9233927c90673c
Ken Irwin <irwinkr@miamioh.edu>
2021-08-30 17:08:44 -0400

    start work on logger

---
+++ scripts/logger.js
@@ -0,0 +1,33 @@
+// const appConf = require('../config/default.json');
+const path = require('path');
+const dayjs = require('dayjs');
+const today = dayjs().format('YYYY-MM-DD');
+const month = dayjs().format('YYYY-MM');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint } = format;
+
+transportsArr = [];
+for (level in appConf.logLevels) {
+  filename = '';
+  if (appConf.logLevels[level] == 'daily') {
+    filename = path.join(__dirname, '..', 'logs', level + '-' + today + '.log');
+  } else if (appConf.logLevels[level] == 'monthly') {
+    filename = path.join(__dirname, '..', 'logs', level + '-' + month + '.log');
+  }
+  if (filename != '') {
+    transportsArr.push(
+      new transports.File({ filename: filename, level: level })
+    );
+  }
+}
+
+const logger = createLogger({
+  format: combine(
+    timestamp(),
+    prettyPrint(),
+    format.json()
+    // format.json((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: transportsArr,
+});
+module.exports = logger;
