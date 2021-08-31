aef8cfef06afc9e51e5a09def004004093b6743c
Ken Irwin <irwinkr@miamioh.edu>
2021-08-31 17:10:06 -0400

    scripts/usageReport: monthly data + distinctUsers, filterable by user type

---
+++ scripts/usageReport.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const Usage = require('../classes/Usage');
+const usage = new Usage();
+const flags = process.argv.slice(2);
+console.log(flags);
+
+let limitByUserType = false;
+if (flags.includes('--student') || flags.includes('--students')) {
+  limitByUserType = 'student';
+} else if (flags.includes('--faculty')) {
+  limitByUserType = 'faculty';
+} else if (flags.includes('--staff')) {
+  limitByUserType = 'staff';
+}
+if (limitByUserType) {
+  console.log('Limiting Data to User Type: ' + limitByUserType);
+}
+
+// Creating a readable stream from file
+// readline module reads line by line
+// but from a readable stream only.
+const file = readline.createInterface({
+  input: fs.createReadStream(
+    path.join(__dirname, '..', 'logs', 'usageLog.txt')
+  ),
+  output: process.stdout,
+  terminal: false,
+});
+
+// Printing the content of file line by
+//  line to console by listening on the
+// line event which will triggered
+// whenever a new line is read from
+// the stream
+
+let data = [];
+
+file.on('line', (line) => {
+  try {
+    let obj = JSON.parse(line);
+    data.push(obj);
+  } catch (err) {}
+});
+
+file.on('close', function () {
+  let firstDate = usage.getFirstDate(data); // do this before applying filters
+  if (limitByUserType) {
+    data = usage.filterDataByUsertype(data, limitByUserType);
+  }
+  console.log('Total Usage:', data.length);
+  console.log('Distinct Users:', usage.distinctUsers(data));
+  console.log('Start Date: ', firstDate);
+  console.log('\n' + 'Monthly Stats:');
+  months = usage.eachMonthSince(firstDate, 'Sep 2021');
+  for (month in months) {
+    let monthData = usage.filterDataByMonth(data, months[month]);
+    let monthUses = monthData.length;
+    let distinctUsers = usage.distinctUsers(monthData);
+    console.log(
+      '* ' +
+        months[month] +
+        ': ' +
+        monthUses +
+        '\t' +
+        ('Distinct users: ' + distinctUsers)
+    );
+  }
+});
