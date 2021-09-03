35ed6b6aa37a329238e85952a3120636513227a4
Ken Irwin <irwinkr@miamioh.edu>
2021-09-03 19:55:39 -0400

    updateSubjectCache creates files for liaison areas that are different from the university subject

--- utilities/updateSubjectCache.js
+++ utilities/updateSubjectCache.js
@@ -4,6 +4,11 @@ subject, it outputs a the combined data from LD&G for that subject into a file:
 /cache/subjects/[SubjectName].js 
 
 These cached subject files are the main data used by the Dashboard.
+
+Note: this file should be refactored. Right now we compile all the Miami subject
+areas, and then also create single-subject files for all the liaison areas. They
+mostly use the same code, but right now it's repeated with only slight variations. 
+Do better! -kri, 2021-09-03
 */
 
 const fs = require('fs');
@@ -66,7 +71,52 @@ subjCodes.forEach((m) => {
             if (err) throw err;
           }
         );
+        console.log(filename);
       }
     });
+  }
+});
+
+// and then do it again for liaison subjects that don't already have a file:
+// come up with a list of all the libguide subject areas
+lgshNames.forEach((subjectName) => {
+  // for each one that doesn't already have a file
+  // create an array of one, e.g. ['Area Studies']
+  // and process it as a subject
+
+  let oneSubjArray = [subjectName];
+  subj = f.findSubjectByName(subjects, oneSubjArray);
+  libns = f.getBestBySubject(librarians, oneSubjArray);
+  pubGuides = f.removeUnpublishedGuides(guides);
+
+  // limit to certain libguide group ids, e.g. exclude admin guides, other campuses, etc.
+  rightGroups = f.removeWrongGroups(pubGuides, allowedGroups);
+
+  gds = f.getBestBySubject(rightGroups, oneSubjArray);
+  dbs = f.getBestBySubject(databases, oneSubjArray, true);
+  let results = {
+    metadata: {
+      sizeof: {
+        librarians: libns.length,
+        guides: gds.length,
+        databases: dbs.length,
+      },
+    },
+    subjects: subj,
+    librarians: libns,
+    guides: gds,
+    databases: dbs,
+  };
+
+  // then write it to a file if such a file wasn't created in the previous step
+  let filename = path.join(
+    __dirname,
+    '..',
+    'cache',
+    'subjects',
+    f.safeFilename(subjectName) + '.json'
+  );
+  if (!fs.existsSync(filename)) {
+    fs.writeFileSync(filename, JSON.stringify(results), { flag: 'wx' });
   }
 });
